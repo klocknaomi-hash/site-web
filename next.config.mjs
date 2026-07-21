d3d307a8b43c38711ae021ef85a18a9cbb08f914
Naomi Klock <naomi@creatabl.com>
2026-07-21 14:59:49 +0200

    fix: remove /contact redirect in next.config.mjs to enable /contact page

--- next.config.mjs
+++ next.config.mjs
@@ -7,26 +7,6 @@ const nextConfig = {
         destination: '/pricing',
         permanent: true,
       },
-      {
-        source: '/mentions-legales',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/cgu',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/confidentialite',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/contact',
-        destination: '/',
-        permanent: false,
-      },
     ];
   },
 };
